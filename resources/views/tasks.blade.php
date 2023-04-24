8f3dbb5315f4726b0907303e98512b3c475738ad
niamx <muhammadniam1999@gmail.com>
2023-04-24 09:01:29 +0700

    edit tasks

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -9,69 +9,79 @@
     @include('common.errors')
 
     <!-- New Task Form -->
-    <form action="/task" method="POST" class="form-horizontal">
-        {{ csrf_field() }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <form action="/task" method="POST" class="">
+                        {{ csrf_field() }}
 
-        <!-- Task Name -->
-        <div class="form-group">
-            <label for="task" class="col-sm-3 control-label">Task</label>
+                        <!-- Task Name -->
+                        <div class="form-group">
+                            <label for="task" class="col-sm-3">Task</label>
 
-            <div class="col-sm-6">
-                <input type="text" name="name" id="task-name" class="form-control">
+                            <div class="col-sm-12">
+                                <input type="text" name="name" id="task-name" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- Add Task Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa-solid fa-plus"></i> Add Task
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    <!-- Current Tasks -->
+                    @if (count($tasks) > 0)
+                    <div class="card">
+                        <div class="card header">
+                            Current Tasks
+                        </div>
+
+                        <div class="panel-body">
+                            <table class="table table-striped task-table">
+
+                                <!-- Table Headings -->
+                                <thead>
+                                    <th>Task</th>
+                                    <th>&nbsp;</th>
+                                </thead>
+
+                                <!-- Table Body -->
+                                <tbody>
+                                    @foreach ($tasks as $task)
+                                    <tr>
+                                        <!-- Task Name -->
+                                        <td class="table-text">
+                                            <div>{{ $task->name }}</div>
+                                        </td>
+
+                                        <td>
+                                            <!-- TODO: Delete Button -->
+                                            <form action="/task/{{ $task->id }}" method="POST">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+
+                                                <button>Delete Task</button>
+                                                <input type="hidden" name="_method" value="DELETE">
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    @endif
+                </div>
             </div>
         </div>
-
-        <!-- Add Task Button -->
-        <div class="form-group">
-            <div class="col-sm-offset-3 col-sm-6">
-                <button type="submit" class="btn btn-primary">
-                <i class="fa-solid fa-plus"></i> Add Task
-                </button>
-            </div>
-        </div>
-    </form>
-</div>
-
-<!-- Current Tasks -->
-@if (count($tasks) > 0)
-<div class="panel panel-default">
-    <div class="panel-heading">
-        Current Tasks
     </div>
 
-    <div class="panel-body">
-        <table class="table table-striped task-table">
-
-            <!-- Table Headings -->
-            <thead>
-                <th>Task</th>
-                <th>&nbsp;</th>
-            </thead>
-
-            <!-- Table Body -->
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <!-- Task Name -->
-                    <td class="table-text">
-                        <div>{{ $task->name }}</div>
-                    </td>
-
-                    <td>
-                        <!-- TODO: Delete Button -->
-                        <form action="/task/{{ $task->id }}" method="POST">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-
-                            <button>Delete Task</button>
-                            <input type="hidden" name="_method" value="DELETE">
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
 </div>
-@endif
+
+
 @endsection
